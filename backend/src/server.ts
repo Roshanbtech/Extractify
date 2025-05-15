bdaedeeae4867af21f6007df2097ba5cada0216a
Roshan Reji <roshanreji987@gmail.com>
2025-05-16 02:09:18 +0530

    Reset dependencies and verified Node.js type definitions

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -73,6 +73,8 @@ app.use(
 );
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// server.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+export default app;
